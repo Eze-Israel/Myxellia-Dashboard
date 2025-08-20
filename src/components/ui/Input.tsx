164ff3d446bc7c8e77c47a239e3de3053bc60b28
Eze-Israel <ezeisraeloluoma123@gmail.com>
2025-08-20 10:12:59 +0100

    adjusted some code to suit Eslint rules

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,8 +1,31 @@
+// import { InputHTMLAttributes, forwardRef } from "react";
+// import { cn } from "@/components/Utils";
+
+// export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+// export const Input = forwardRef<HTMLInputElement, InputProps>(function Input({ className, ...props }, ref) {
+//   return <input ref={ref} className={cn("h-10 w-full rounded-xl border border-slate-200 bg-gray-100 px-3 outline-none focus:ring-2 focus:ring-brand/30", className)} {...props} />;
+// });
+
+
 import { InputHTMLAttributes, forwardRef } from "react";
 import { cn } from "@/components/Utils";
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(function Input({ className, ...props }, ref) {
-  return <input ref={ref} className={cn("h-10 w-full rounded-xl border border-slate-200 bg-gray-100 px-3 outline-none focus:ring-2 focus:ring-brand/30", className)} {...props} />;
-});
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={cn(
+          "h-10 w-full rounded-xl border border-slate-200 bg-gray-100 px-3 outline-none focus:ring-2 focus:ring-brand/30",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
